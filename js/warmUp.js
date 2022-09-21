f1f9acc1121565adca45bb9d7399432e2283a88c
Miles Raker <milesraker@gmail.com>
2022-09-21 07:21:59 -0700

    9/21 warmup

---
+++ js/warmUp.js
@@ -0,0 +1,12 @@
+/*Area of a Triangle: Write a JavaScript function that accepts the 3 lengths of a triangle and outputs the triangle’s area.*/
+function triangleArea(x, y, z){
+    let p = (x + y + z)/2;
+    return Math.sqrt(p*(p-a)*(p-b)*(p-c));
+}
+
+/*Reverse a String: Write a JS function that accepts a string and returns the string in reverse. example input: codeup, example output: puedoc*/
+
+function reverseString(input){
+    return input.reverse();
+}
+
