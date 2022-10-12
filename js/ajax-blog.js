c42410839d0712065a0e095a2a7d2cb686923caa
Miles Raker <milesraker@gmail.com>
2022-10-12 07:55:12 -0700

    JQuery:Ajax:MapboxAPI: Exercise steps 1 and 2 complete

--- js/ajax-blog.js
+++ js/ajax-blog.js
@@ -3,7 +3,8 @@
 $.get("data/blog.json").done(function(data){
     let htmlString = "";
     data.forEach(function(bPost){
-        htmlString += `<div class="bPost card shadow"><h2>${bPost.title} <small class="text-muted">${bPost.date}</small></h2><p>${bPost.content}</p></div><br>`;
+        htmlString += `<div class="bPost card shadow"><h2>${bPost.title} <small class="text-muted">${bPost.date}</small>
+        </h2><p>${bPost.content}</p></div><br>`;
     })
     $("#posts").html(htmlString);
 });
